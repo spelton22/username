cf7059702623b083cb7c15a9ca64f12aa8f4a2cc
Sophie Pelton <spelton@chapman.edu>
2020-11-09 10:43:52 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# username.sh
+# Sophie Pelton
+
+echo "Enter a username: "
+echo "use lower case characters."
+echo "it must start with a lower case character."
+echo "it must contain at least 3 but not more than 12 characters."
+
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username"
+	echo "Enter a username: "
+	read USERNAME
+done
+echo "Thank you"
